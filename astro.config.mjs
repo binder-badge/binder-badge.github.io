c4713a34658c8111c3e9c724e6dec8be5032f4f5
binder-badge <89ebgie2@anonaddy.me>
2025-08-25 17:43:44 -0400

    add some new articles + change title card

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,10 @@ export default defineConfig({
 					label: 'Homelab',
 					autogenerate: { directory: 'homelab' }						
 				},
+				{
+					label: 'Shinanegans',
+					autogenerate: { directory: 'shinanegans' }						
+				},
 			],
 		}),
 	],
---
+++ src/content/docs/shinanegans/laptops/scuffed-desktop.md
@@ -0,0 +1,4 @@
+---
+title: Making a Scuffed Desktop
+description: Documentation on how I made an old laptop 
+---
